bb1719f61885868cffe90b62ba909e0db96d0693
Eurvin Morman <eurvin@gmail.com>
2023-05-14 00:56:53 +0200

    - removed the secondary button because it is not needed

--- packages/frontend/src/components/about/AboutUs.tsx
+++ packages/frontend/src/components/about/AboutUs.tsx
@@ -29,7 +29,6 @@ export const AboutUs: FC = () => {
             <Button colorScheme="blue" mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button variant="ghost">Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
